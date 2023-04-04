7c0120546337cf35ba2f70832eb734826a45de54
jwj1342 <739151564@qq.com>
2023-04-04 18:06:05 +0800

    测试是否可以同时进行开发。

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'This is a test for commit and push',
       theme: ThemeData(
         // This is the theme of your application.
         //
