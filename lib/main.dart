4ffcf4ccf087e3c331908e14a3e85eef29f9b220
jwj1342 <739151564@qq.com>
2023-04-19 15:21:29 +0800

    完成导航栏的开发，在导航栏中加入登录栏目方便开发。

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:vangogh/Create/creat_page.dart';
+import 'package:vangogh/Auth/login_page.dart';
+import 'package:vangogh/Create/create_page.dart';
 import 'package:vangogh/Home/home_page.dart';
 import 'package:vangogh/My/my_page.dart';
 
@@ -35,13 +36,10 @@ class MyStatefulWidget extends StatefulWidget {
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const List<Widget> _widgetOptions = <Widget>[
-    // Text(
-    //   'Index 0: Home',
-    //   style: optionStyle,
-    // ),
     HomePage(),
     CreatePage(),
     MyPage(),
+    LoginPage(),
   ];
 
   void _onItemTapped(int index) {
@@ -57,22 +55,32 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: '首页',
+            backgroundColor: Color.fromRGBO(177, 182, 75, 0.9),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
+            icon: Icon(Icons.touch_app),
             label: '创作',
+            backgroundColor: Color.fromRGBO(111, 136, 120, 1),
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
+            icon: Icon(Icons.supervisor_account),
             label: '我的',
+            backgroundColor: Color.fromRGBO(173, 170, 196, 1),
+          ),
+          BottomNavigationBarItem(
+            //这个登录的几面接口仅供开发使用，最后会移除。
+            icon: Icon(Icons.login),
+            label: '登录',
+            backgroundColor: Colors.red,
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Colors.black,
         onTap: _onItemTapped,
       ),
     );
