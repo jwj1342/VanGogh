87d711ad57a654356abed8f29176a3a0f1f8539c
jwj1342 <739151564@qq.com>
2023-04-22 22:40:03 +0800

    修改zxd代码，提取并重构了三个按钮，使得页面逻辑更加清晰。

--- lib/Create/create_page.dart
+++ lib/Create/create_page.dart
@@ -3,123 +3,105 @@ import 'package:flutter/material.dart';
 
 class CreatePage extends StatelessWidget {
   const CreatePage({super.key});
-  //const Placeholder();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor:Color(0xf3a7bbae),//背景
-        body: Padding(
-          padding: EdgeInsets.symmetric(vertical:30,horizontal: 30),
-            child:Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  child: Text(
-                  '时光清浅处',
-                  style: TextStyle(
-                    color: Color(0xff252323),
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w600
-                  ),
-                  )
-                ),
-                Container(
-                  child: Text(
-                  '一步一安然',
-                  style: TextStyle(
-                    color: Color(0xff252323),
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w600
-                 ),
-                 )
-               ),
-              const SizedBox(//分割线
-                child: Divider(
+      backgroundColor: const Color(0xf3a7bbae), //背景
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Text(
+              '时光清浅处',
+              style: TextStyle(
+                  color: Color(0xff252323),
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.w600),
+            ),
+            const Text(
+              '一步一安然',
+              style: TextStyle(
+                  color: Color(0xff252323),
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              //分割线
+              child: Divider(
                 color: Colors.black26,
                 thickness: 5,
-                ),
               ),
-              Wrap(
-                spacing:8,
-                runSpacing:8,
-                children:<Widget>[
-                  Container(//三个button
-                    padding:EdgeInsets .fromLTRB( 0,10, 4, 30),
-                    child:ElevatedButton (
-                      child:Text('全部'),
-                      onPressed:(){},
-                      style:ButtonStyle(
-                        backgroundColor:MaterialStateProperty.all(CupertinoColors.inactiveGray),
-                        foregroundColor:MaterialStateProperty.all(Colors.black),
-                        textStyle:MaterialStateProperty.all(TextStyle(fontSize:14)),
-                        shape:MaterialStateProperty.all(
-                          StadiumBorder(
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(//三个button
-                    padding:EdgeInsets .fromLTRB( 0,10, 4, 30),
-                    child: ElevatedButton (
-                      child:Text('已编辑'),
-                        onPressed:(){
-
-                      },
-                      style:ButtonStyle(
-                        backgroundColor:MaterialStateProperty.all(CupertinoColors.inactiveGray),
-                        foregroundColor:MaterialStateProperty.all(Colors.black),
-                        textStyle:MaterialStateProperty.all(TextStyle(fontSize:14)),
-                        shape:MaterialStateProperty.all(
-                          StadiumBorder(
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(//三个button
-                    padding:EdgeInsets .fromLTRB( 0,10, 50, 30),
-                    child: ElevatedButton (
-                      child:Text('已发布'),
-                      onPressed:(){},
-                      style:ButtonStyle(
-                        backgroundColor:MaterialStateProperty.all(CupertinoColors.inactiveGray),
-                        foregroundColor:MaterialStateProperty.all(Colors.black),
-                        textStyle:MaterialStateProperty.all(TextStyle(fontSize:14)),
-                        shape:MaterialStateProperty.all(
-                          StadiumBorder(
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                   Wrap(//通过流式布局排列图片
-                     spacing:2,
-                     runSpacing:5,
-                     children :<Widget>[
-
-
-                     ],
-                   )
-               ],
-             ),
-           ],
-          ),
+            ),
+            Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              children: <Widget>[
+                CreatedSelectButtom(
+                  onPress: () {},
+                  text: "全部",
+                ),
+                CreatedSelectButtom(
+                  onPress: () {},
+                  text: "已编辑",
+                ),
+                CreatedSelectButtom(
+                  onPress: () {},
+                  text: "已发布",
+                ),
+                Wrap(
+                  //通过流式布局排列图片
+                  spacing: 2,
+                  runSpacing: 5,
+                  children: <Widget>[],
+                )
+              ],
+            ),
+          ],
         ),
-        floatingActionButton://加号
-        FloatingActionButton(
-        backgroundColor:Colors.orangeAccent,
-        onPressed:(){},
-        child:IconButton(
-        icon:Icon(Icons.add),
-        onPressed:(){},
+      ),
+      floatingActionButton: //加号
+          FloatingActionButton(
+        backgroundColor: Colors.orangeAccent,
+        onPressed: () {},
+        child: IconButton(
+          icon: const Icon(Icons.add),
+          onPressed: () {},
         ),
       ),
     );
   }
-
 }
 
+class CreatedSelectButtom extends StatelessWidget {
+  const CreatedSelectButtom({
+    super.key,
+    required this.onPress,
+    required this.text,
+  });
+
+  final VoidCallback onPress;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //三个button
+      padding: const EdgeInsets.fromLTRB(0, 10, 4, 30),
+      child: ElevatedButton(
+        onPressed: onPress,
+        style: ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all(CupertinoColors.inactiveGray),
+          foregroundColor: MaterialStateProperty.all(Colors.black),
+          textStyle: MaterialStateProperty.all(const TextStyle(fontSize: 14)),
+          shape: MaterialStateProperty.all(
+            const StadiumBorder(),
+          ),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
