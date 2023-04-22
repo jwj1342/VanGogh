6e5d8a3d129d3871df49521cd2b9d789920c777a
jwj1342 <739151564@qq.com>
2023-04-22 16:29:26 +0800

    整理了首页的小组件。

--- lib/Home/home_page.dart
+++ lib/Home/home_page.dart
@@ -2,183 +2,120 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    //TODO: 1.使用Padding来设置顶部两个文字间距，让他俩变得美观一点
-    //TODO: 2.对整个首页的Stack进行优化，让他们不会贴到两边，就是之前参考线的位置
-    //TODO: 3.图片全部使用占位符代替，等到后面再进行替换
     return Scaffold(
         backgroundColor: const Color(0xA6ECE8B9),
         body: Padding(
-            padding: EdgeInsets.symmetric(vertical:30,horizontal: 30),
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Container(
-                      child: Text(
-                        '生有热烈',
-                        style: TextStyle(
-                            color: Color(0xff252323),
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w600
-                        ),
-                      )
+                  const Text(
+                    '生有热烈',
+                    style: TextStyle(
+                        color: Color(0xff252323),
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const Text(
+                    '藏与俗常',
+                    style: TextStyle(
+                        color: Color(0xff252323),
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.w600),
                   ),
                   Container(
-                      child: Text(
-                        '藏与俗常',
-                        style: TextStyle(
-                            color: Color(0xff252323),
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.w600
-                        ),
-                      )
-                  ),
-                  Container(
-                      padding: EdgeInsets.only(top: 40,bottom: 20),
-                      child: Text(
+                      padding: const EdgeInsets.only(top: 40, bottom: 20),
+                      child: const Text(
                         '不同的流派画作',
                         style: TextStyle(
                           color: Color(0xff252323),
                           fontSize: 20.0,
                         ),
-                      )
-                  ),
-                  Container(
+                      )),
+                  SizedBox(
                     height: 180,
-                    child: new ListView(
+                    child: ListView(
                         scrollDirection: Axis.horizontal,
-                        children:<Widget> [
-                          new InkWell(
-                            onTap: (){},
-                            child: new Container(
-
-                              decoration: ShapeDecoration(
-                                  image: const DecorationImage(
-                                      image: AssetImage("images/placeholder.jpg"),
-                                      fit: BoxFit.fitWidth),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadiusDirectional.circular(20))),
-                              width: 250,
-                              child: const Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "印象派",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w700),
-                                  ),
-                                ),
-                              ),
-                            ),
+                        children: <Widget>[
+                          CustomWellHorizonal(
+                            onTap: () {},
+                            imagePath: "assets/images/placeholder.jpg",
+                            text: "印象派",
                           ),
-                          new Container(
+                          Container(
                             width: 10,
                           ),
-                          new InkWell(
-                            onTap: (){},
-                            child: new Container(
-                              decoration: ShapeDecoration(
-                                  image: const DecorationImage(
-                                      image: AssetImage("images/placeholder.jpg"),
-                                      fit: BoxFit.fitWidth),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadiusDirectional.circular(20))),
-                              width: 250,
-                              child: const Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "后印象派",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w700),
-                                  ),
-                                ),
-                              ),
-                            ),
+                          CustomWellHorizonal(
+                            onTap: () {},
+                            imagePath: "assets/images/placeholder.jpg",
+                            text: "后印象派",
                           ),
-                          new Container(
+                          Container(
                             width: 10,
                           ),
-                          new InkWell(
-                            onTap: (){},
-                            child: new Container(
-                              decoration: ShapeDecoration(
-                                  image: const DecorationImage(
-                                      image: AssetImage("images/placeholder.jpg"),
-                                      fit: BoxFit.fitWidth),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadiusDirectional.circular(20))),
-                              width: 250,
-                              child: const Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "抽象派",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w700),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          new Container(
-                            width: 10,
-                          ),
-                          new InkWell(
-                            onTap: (){},
-                            child: new Container(
-                              decoration: ShapeDecoration(
-                                  image: const DecorationImage(
-                                      image: AssetImage("images/placeholder.jpg"),
-                                      fit: BoxFit.fitWidth),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadiusDirectional.circular(20))),
-                              width: 250,
-                              child: const Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Text(
-                                    "实派",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w700),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ]
-                    ),
+                        ]),
                   ),
                   Container(
-                      padding: EdgeInsets.only(top: 40,bottom: 20),
-                      child: Text(
+                      padding: const EdgeInsets.only(top: 40, bottom: 20),
+                      child: const Text(
                         '用户的创作',
                         style: TextStyle(
                           color: Color(0xff252323),
                           fontSize: 20.0,
                         ),
-                      )
-                  ),
-                  Container(
-                      height: 200,
-                      child: Placeholder())
-                ]
-            )
-        ));
+                      )),
+                  const SizedBox(height: 200, child: Placeholder())
+                ])));
+  }
+}
+
+class CustomWellHorizonal extends StatelessWidget {
+  const CustomWellHorizonal({
+    super.key,
+    required this.onTap,
+    required this.imagePath,
+    required this.text,
+  });
+
+  final VoidCallback onTap;
+  final String imagePath;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: ShapeDecoration(
+          image: DecorationImage(
+            image: AssetImage(imagePath),
+            fit: BoxFit.fitWidth,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusDirectional.circular(20),
+          ),
+        ),
+        width: 250,
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              text,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
 
