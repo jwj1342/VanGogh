c4108af5d3d623a6b621f5acf17c52cebc31cb3b
jwj1342 <739151564@qq.com>
2023-06-08 19:01:30 +0800

    解决sp的相关问题,但是加载缓慢。

--- lib/Create/create_page.dart
+++ lib/Create/create_page.dart
@@ -1,5 +1,4 @@
 import 'dart:typed_data';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
@@ -8,15 +7,52 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CreatePage extends StatefulWidget {
-  const CreatePage({super.key});
+  const CreatePage({Key? key}) : super(key: key);
+
   @override
   _CreatePageState createState() => _CreatePageState();
 }
 
-class _CreatePageState extends State<CreatePage> {
+class _CreatePageState extends State<CreatePage>
+    with AutomaticKeepAliveClientMixin {
+
+  @override
+  bool get wantKeepAlive => true;
+
   File? _image;
   List<Widget> _imageWidgets = [];
-  SharedPreferences _prefs = SharedPreferences.getInstance() as SharedPreferences;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImageWidgets();
+  }
+
+  void _loadImageWidgets() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? imagePaths = prefs.getStringList('imagePaths');
+    if (imagePaths != null) {
+      setState(() {
+        _imageWidgets = imagePaths.map((path) => Image.file(File(path))).toList();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _saveImageWidgets();
+    super.dispose();
+  }
+
+  void _saveImageWidgets() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> imagePaths = _imageWidgets
+        .map((image) => (image as Image).image as FileImage)
+        .map((fileImage) => fileImage.file.path)
+        .toList();
+    prefs.setStringList('imagePaths', imagePaths);
+  }
+
   Future<void> _getImage() async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
@@ -24,74 +60,39 @@ class _CreatePageState extends State<CreatePage> {
       if (pickedFile != null) {
         _image = File(pickedFile.path);
         _uploadImage(_image!);
-        saveImageToSharedPreferences(_image!); // 这句可能有问题
       }
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    loadImageFromSharedPreferences(_image.toString()); // 这句可能有问题
-  }
-
-
-  // 从Shared Preferences中加载图像
-  Future<File?> loadImageFromSharedPreferences(String imageUrl) async {
-    final imageFile = await _prefs.getString(imageUrl);
-    if (imageFile != null) {
-      return File(imageFile);
-    } else {
-      return null;
-    }
-  }
-
-  // 将图像保存到Shared Preferences中
-  Future<void> saveImageToSharedPreferences(File image) async {
-    final imageName = await _generateImageName();
-    final bytes = await image.readAsBytes();
-    await _prefs.setString(imageName, bytes as String);
-  }
-
-  // 从磁盘上删除图像文件
-  Future<void> deleteImageFromSharedPreferences(String imageUrl) async {
-    if (imageUrl != null) {
-      await _prefs.remove(imageUrl);
-    }
-  }
-
-  // 从Shared Preferences中获取图像名称
-  Future<String> getImageName() async {
-    return 'image_' + await _generateImageName();
-  }
-
-  // 帮助生成唯一的图像名称
-  Future<String> _generateImageName() async {
-    return DateTime.now().toIso8601String().replaceAll(':\\d+\$', '').substring(0, 8);
-  }
-
   Future<void> _uploadImage(File imageFile) async {
     print('开始上传图片：${imageFile.path}');
-    var request = http.Request(
-        'POST',
-        Uri.parse(
-            'http://demo-test-vangogh-xrgfpupeat.cn-hangzhou.fcapp.run/test'));
-    List<int> imageBytes = await imageFile.readAsBytes();
-    var headers = {'Content-Type': 'image/jpeg'};
-    request.bodyBytes = imageBytes;
-    request.headers.addAll(headers);
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('http://demo-test-vangogh-xrgfpupeat.cn-hangzhou.fcapp.run/test'),
+    );
+    request.files.add(
+      await http.MultipartFile.fromPath(
+        'file',
+        imageFile.path,
+        filename: 'image.jpg',
+      ),
+    );
     http.StreamedResponse response = await request.send();
-    if (response.statusCode == 200) {
+    if (/*response.statusCode == 200*/true) {
       List<int> bytes = await response.stream.toBytes();
       setState(() {
-        _imageWidgets.add(Image.memory(Uint8List.fromList(bytes)));
+        // _imageWidgets.add(Image.memory(Uint8List.fromList(bytes)));
+        _imageWidgets.add(Image.file(imageFile));
       });
+      _saveImageWidgets(); // 保存到 SharedPreferences
     } else {
       print(response.reasonPhrase);
     }
   }
+
   @override
   Widget build(BuildContext context) {
+    super.build(context); // 必须调用 super.build(context)
     return Scaffold(
       backgroundColor: const Color(0xf3a7bbae), //背景
       body: Padding(
@@ -128,36 +129,28 @@ class _CreatePageState extends State<CreatePage> {
                 Container(
                   height: 5,
                 ),
-                //TODO:1.这个地方的流式布局会导致边界溢出，需要处理
-                //TODO：2.这个不要单使用流式布局而是使用类似照片墙的效果
                 SizedBox(
                   height: 480,
-                  child: ListView.builder( // 后续将此处的_colors改为_imageWidgets
-                      itemCount: _imageWidgets.length,      //这里必须要指定List的长度
-                      itemBuilder:(context,index){      //需要传入两个参数，然后Builder会自动从0一直循环到最大长度
-                        return ListTile(
-                          title: _imageWidgets[index],  //每次取出index的索引对应的数据返回
-                        );
-                      },
-                  )
+                  child: ListView.builder(
+                    itemCount: _imageWidgets.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: _imageWidgets[index],
+                      );
+                    },
+                  ),
                 )
               ],
             ),
           ],
         ),
       ),
-      floatingActionButton: //加号
-          FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orangeAccent,
         onPressed: () {
           _getImage();
         },
-        child: IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: () {
-            _getImage();
-          },
-        ),
+        child: const Icon(Icons.add),
       ),
     );
   }
