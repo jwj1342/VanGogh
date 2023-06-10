c4e29c05d4eef59c742a500edee7e2dee978f9e4
dshgz <1290325818@qq.com>
2023-06-10 22:51:54 +0800

    文章样式

--- lib/Home/ArticlePage.dart
+++ lib/Home/ArticlePage.dart
@@ -14,9 +14,22 @@ class ArticlePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xA6ECE8B9), // 修改背景颜色
       appBar: AppBar(
+        title: Text(
+          '印象派介绍',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: const Color(0xFF634634), // 设置导航栏文字颜色为黑色
+          ),
+        ),
+        centerTitle: true,
+        // 设置导航栏文字居中
+        backgroundColor: Color(0xffECE8B9),
+        //elevation: 0, // 去除底部阴影
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
+          color: const Color(0xFF634634),
           onPressed: () {
             Navigator.pop(context); // 返回上一个页面
           },
@@ -28,7 +41,23 @@ class ArticlePage extends StatelessWidget {
           if (snapshot.hasData) {
             return Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Markdown(data: snapshot.data.toString()),
+              child: Markdown(
+                data: snapshot.data.toString(),
+                styleSheet: MarkdownStyleSheet(
+                  // 设置Markdown样式
+                  h1: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xFF634634),
+                  ),
+                  h2: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: const Color(0xFF634634),
+                  ),
+                  p: TextStyle(fontSize: 16.0),
+                ),
+              ),
             );
           } else if (snapshot.hasError) {
             return const Center(child: Text('Failed to load article'));
